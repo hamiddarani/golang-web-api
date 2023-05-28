d177803493f2a7ba84063190fd920749c97420ba
Hamid Darani <hamiddarani367@gmail.com>
2023-05-28 20:39:13 +0330

    Add custom validations

--- src/api/api.go
+++ src/api/api.go
@@ -2,12 +2,17 @@ package api
 
 import (
 	"golang-web-api/api/routers"
+	"golang-web-api/api/validations"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 func InitServer() {
 	r := gin.New()
+	RegisterValidators()
 	r.Use(gin.Logger(), gin.Recovery())
 
 	v1 := r.Group("/api/v1")
@@ -17,4 +22,14 @@ func InitServer() {
 	}
 
 	r.Run(":5000")
+}
+
+func RegisterValidators() {
+	val, ok := binding.Validator.Engine().(*validator.Validate)
+	if ok {
+		err := val.RegisterValidation("mobile", validations.IranianMobileNumberValidator, true)
+		if err != nil {
+			log.Print(err.Error())
+		}
+	}
 }
--- src/go.mod
+++ src/go.mod
@@ -2,7 +2,10 @@ module golang-web-api
 
 go 1.18
 
-require github.com/gin-gonic/gin v1.9.0
+require (
+	github.com/gin-gonic/gin v1.9.0
+	github.com/go-playground/validator/v10 v10.11.2
+)
 
 require (
 	github.com/bytedance/sonic v1.8.0 // indirect
@@ -10,7 +13,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
